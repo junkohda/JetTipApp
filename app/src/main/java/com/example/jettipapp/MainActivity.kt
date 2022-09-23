99621a166e50a067bd35243334001df7e200a576
jun.kohda <>
2022-09-23 21:45:24 +0900

    89まで実装

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -4,10 +4,8 @@ import android.app.assist.AssistContent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,8 +13,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettipapp.ui.theme.JetTipAppTheme
@@ -26,7 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello Again")
+                TopHeader()
             }
         }
     }
@@ -41,31 +42,42 @@ fun MyApp(content: @Composable () -> Unit){
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun TopHeader(){
+fun TopHeader(totalPerPerson: Double = 0.0){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .height(150.dp)
-        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))))
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    )
         {
-        Column() {
-
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person", style = MaterialTheme.typography.h5)
+            Text(text = "¥$total", style = MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
         }
 
     }
 }
 
-
-
-
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetTipAppTheme {
-        MyApp {
-            Text(text = "Hello Again!")
+fun MainContent(){
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column() {
+
         }
+        
     }
 }
